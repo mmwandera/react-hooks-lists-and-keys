e2ddcca78e8bd0d8dedf7d1dc06864dd7f4212b6
mmwandera <mmwandera74@gmail.com>
2023-11-21 13:54:43 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,37 @@ function App() {
   return (
     <div>
       <ColorList />
+      <ColorList2 />
+    </div>
+  );
+}
+
+function ColorList2() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+    "deepskyblue",
+    "crimson",
+    "indigo",
+    "olive",
+    "peru",
+    "plum",
+    "violet"
+  ];
+
+  const colorElements = colors.map((color) => {
+    return <li style={{ color: color }}>{color}</li>;
+  });
+  return (
+    <div>
+      <h1>Top {colors.length} CSS Colors (ColorList2) </h1>
+      <ol>
+        {/* display the array of <li> elements here! */}
+        {colorElements}
+      </ol>
     </div>
   );
 }
--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,17 +7,21 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "deepskyblue",
+    "crimson"
   ];
 
   return (
     <div>
-      <h1>Top 5 CSS Colors</h1>
+      <h1>Top {colors.length} CSS Colors (ColorList1)</h1>
       <ol>
         <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
         <li style={{ color: colors[4] }}>{colors[4]}</li>
+        <li style={{ color: colors[5] }}>{colors[5]}</li>
+        <li style={{ color: colors[6] }}>{colors[6]}</li>
       </ol>
     </div>
   );
